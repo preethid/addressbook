25d6d6440ea254086f6a55e45b943b7145304661
Preethi <preethidovala@gmail.com>
2021-10-04 23:04:44 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,18 @@
-def compile() {
-   
-    sh 'mvn compile'
-} 
-
-def UnitTest() {
-  
-    sh 'mvn test'
-} 
-
-def package() {
- 
-  sh 'mvn package'
-} 
+def buildwar(){
+    echo "building the application..."
+    git 'https://github.com/preethid/addressbook.git'
+    sh 'mvn package'
+}
+def buildImage(){
+    echo "deploying the application..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub',passwordVariable: 'PASS',usernameVariable: 'USER')]){
+    sh 'sudo docker build -t devopstrainer/myrepoprivate:jenkinsjob .'
+    sh 'sudo docker login -u $USER -p $PASS'
+    //sh "echo $PASS | sudo docker login -u $USER --password-stdin"
+    sh 'sudo docker push devopstrainer/myrepoprivate:jenkinsjob'
+}
+def deployApp(){
+    echo 'deploying the app..'
+}
 
 return this
