21065de3f4c4d07b040f343e2796e60f77677e7e
b <l>
2022-03-02 10:04:47 +0530

    k8s files

--- server-script.sh
+++ server-script.sh
@@ -2,15 +2,15 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-if [ -d "addressbook-1" ]
+if [ -d "addressbook" ]
 then 
   echo "repo is cloned and exists"
-  cd /home/ec2-user/addressbook-1
+  cd /home/ec2-user/addressbook
   git pull origin democicd
 else
-  git clone https://github.com/preethid/addressbook-1.git
+  git clone https://github.com/preethid/addressbook.git
 fi
-cd /home/ec2-user/addressbook-1
+cd /home/ec2-user/addressbook
 mvn package
 sudo yum install docker -y
 sudo systemctl start docker
