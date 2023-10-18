bb54debaf9725b0a25c300c666a842e5ba693fe3
Your Name <you@example.com>
2023-10-18 07:12:35 +0530

    ssh

--- server-config.sh
+++ server-config.sh
@@ -2,7 +2,7 @@ sudo yum install git -y
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install maven -y
 
-if [-d "addressbook" ]
+if [ -d "addressbook" ]
 then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook
