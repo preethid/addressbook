ba8042fcb6ebc78694645403b77681b5b6a814df
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-05-21 08:08:26 +0530

    slave config

--- server-config.sh
+++ server-config.sh
@@ -1,5 +1,5 @@
 sudo yum install git -y
-sudo yum install java -y
+sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install maven -y
 # sudo yum install docker -y
 # sudo systemctl start docker
