ddbce1a5bc22204c47c09d3cf57da77117c8298c
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-06-27 06:31:59 +0530

    slave2 added

---
+++ server-script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+sudo yum install git -y
+sudo yum insatll java-1.8.0-openjdk-devel -y
+sudo yum install maven -y
+
+
+if [ -d "addressbook" ]
+then
+   echo "repo is cloned and exists"
+   cd addressbook
+   git pull origin devops-1/july
+else
+   git clone https://github.com/preethid/addressbook.git
+   cd addressbook
+fi
+
+mvn test
