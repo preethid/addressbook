c89b222da0fc4f76a4656493915024974a2ee867
rajavelugit <rajavelu8111@gmail.com>
2024-07-02 00:16:34 +0530

    shell script added

---
+++ server-script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo yum install git -y
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install maven -y
+if [ -d "addressbook" ]
+then 
+echo "repo is coloned and exist"
+cd addressbook
+git pull origin devops-1/july
+else
+git clone https://github.com/rajavelugithub/addressbook.git
+cd addressbook
+fi
+mvn test
